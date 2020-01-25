b23e69de14bea9dcb2bd6a15846fde63971e8e92
Adam Armendariz <armenaa652@gmail.com>
2020-01-24 21:05:54 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,43 +1,76 @@
+var sketch = function (b) {
+    b.banner;
+    b.setup = function () {
+        b.banner = b.createCanvas(transX, transY);
+        b.banner.position(0, 0);
+        b.banner.style('z-index', '-1');
+        b.fill(80, 0, 0)
+        b.rect(0, -2, transX, transY + 2)
+    }
+    b.draw = function () {
+    }
+}
+var myp5 = new p5(sketch);
+
 let rows;
 let cols;
 
-let time;
 
 let grid = [];
-let size = 20;
+let size = 10;
 
 let canvas;
 let width;
 let height;
 
-let transX = 280;
-let transY = 188;
+let transX = 220;
+let transY = 152;
 
+let first = true;
+let resize = false;
+let time;
 function setup() {
     //noStroke()
-    width = windowWidth;
+    width = windowWidth - transX;
     height = transY;
-    rows = floor(height / size);
-    cols = floor(width / size);
     canvas = createCanvas(width, height);
-    canvas.position(0, 0);
+    canvas.position(transX, 0);
     canvas.style('z-index', '-1');
     grid = createGrid();
     frameRate(10);
 }
 
 function draw() {
+    if (first) {
+        if (resize) {
+            time = millis();
+        } else {
+            if (time + 100 < millis()) {
+                grid = createGrid();
+                first = false;
+            }
+        }
+    }
+    canvas.position(0, 0);
     fill(80, 0, 0)
-    rect(0, 0, windowWidth, transY)
+    rect(0, 0, windowWidth, transY);
+    canvas.position(transX, 0);
     drawGrid();
     grid = updateGrid();
+    resize = false;
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, height);
+    resize = true;
+    first = true;
 }
 
 function createGrid() {
+    width = windowWidth - transX;
+    height = transY;
+    rows = floor(height / size);
+    cols = floor(width / size);
     let arr = new Array(rows);
     for (let i = 0; i < rows; i++) {
         arr[i] = new Array(cols);
@@ -55,8 +88,8 @@ function drawGrid() {
                 fill(255);
             else
                 noFill();
-            rect(j * size + transX, i * size, size, size)
-            //rect(width / 2 + j * size - cols / 2 * size, height / 2 + i * size - rows / 2 * size, size, size);
+            //rect(j * size + transX, i * size, size, size)
+            rect(width / 2 + j * size - cols / 2 * size, height / 2 + i * size - rows / 2 * size, size, size);
         }
     }
 }
@@ -90,10 +123,12 @@ function updateCell(i, j) {
 }
 
 function mouseMoved() {
-    let xpos = floor(((mouseX - transX) - width / 2 + cols / 2 * size) / size);
+    //let xpos = floor(((mouseX - transX) - width / 2 + cols / 2 * size) / size);
+    let xpos = floor((mouseX - width / 2 + cols / 2 * size) / size);
     let ypos = floor((mouseY - height / 2 + rows / 2 * size) / size);
     if ((0 <= xpos && xpos < cols) && (0 <= ypos && ypos < rows)) {
         grid[ypos][xpos] = true;
     }
 
 }
+
